766294b575956a47a238a905203e69b4c9870882
DiriARG <matidirisio@gmail.com>
2024-10-06 16:53:07 -0300

    Empezamos con los inserts

---
+++ trailerflix_inserts.sql
@@ -0,0 +1,43 @@
+USE trailerflix;
+
+-- Insertar las categorías.
+INSERT INTO categorias (nombre) VALUES
+('Serie'),
+('Película');
+
+-- Insertar géneros.
+INSERT INTO generos (nombre) VALUES
+('Sci-Fi'),
+('Fantasía'),
+('Acción'),
+('Drama'),
+('Ficción'),
+('Sucesos'),
+('Misterio'),
+('Suceso Real'),
+('Crimen'),
+('Suspenso'),
+('Aventura'),
+('Comedia'),
+('Familia'),
+('Western'),
+('Tecnología'),
+('Futurista'),
+('Terror'),
+('Historia'),
+('Intriga'),
+('¿Ficción?'),
+('Ciencia ficción'),
+('Romance'),
+('Música'),
+('Musical'),
+('Animación');
+
+-- Insertar actores.
+INSERT INTO actores (nombre, apellido) VALUES
+('Pedro', 'Pascal'),
+('Carl', 'Weathers'),
+('Misty', 'Rosas'),
+('Chris', 'Bartlett'),
+('Rio', 'Hackford'),
+('Giancarlo', 'Esposito'),
