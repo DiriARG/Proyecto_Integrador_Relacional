a0ae5fdaecb4ac4c8c4a897732cedff2d2049213
DiriARG <matidirisio@gmail.com>
2024-10-11 16:03:55 -0300

    Correción ortográfica

--- ejemplo_consulta_sql.sql
+++ ejemplo_consulta_sql.sql
@@ -1,6 +1,5 @@
 -- Este archivo es para probar su correcta ejecución después de haber realizado todas las inserciones de datos en las tablas de la base de datos "trailerflix".
 
-
 USE trailerflix;
 
 -- Seleccionamos los datos a mostrar...
@@ -8,7 +7,7 @@ SELECT
     c.titulo AS 'Título',
 	cat.nombre AS 'Categoría',
     c.resumen AS 'Resumen',
-    COALESCE(c.temporadas, c.duracion) AS 'Temporadas/Duración', -- Se utiliza para devolver el primer valor no nulo en una lista de expresiones. Primero evalua si c.temporadas es nulo, entonces devuelve el valor de c.duracion; si no lo es entonces devuelve el valor de c.temporadas.
+    COALESCE(c.temporadas, c.duracion) AS 'Temporadas/Duración', -- Se utiliza para devolver el primer valor no nulo en una lista de expresiones. Primero evaluá si c.temporadas es nulo, entonces devuelve el valor de c.duracion; si no lo es entonces devuelve el valor de c.temporadas.
     GROUP_CONCAT(DISTINCT g.nombre SEPARATOR ', ') AS 'Géneros', -- Agrupa y concatena los géneros de la película o serie.
     GROUP_CONCAT(DISTINCT CONCAT(a.nombre, ' ', a.apellido) SEPARATOR ', ') AS 'Actores', -- Agrupa y concatena los actores, mostrando su nombre y apellido.
     c.trailer AS 'Tráiler'
