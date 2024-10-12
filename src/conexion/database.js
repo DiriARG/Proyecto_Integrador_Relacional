af322b41930e8fdeef84c7e6729015d628032697
DiriARG <matidirisio@gmail.com>
2024-10-12 14:53:54 -0300

    Creamos la conexión a la BD

--- src/conexion/database.js
+++ src/conexion/database.js
@@ -1,18 +1,38 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'trailerflix'
+// Importamos la dependencia Sequelize para interactuar con la base de datos.
+const { Sequelize } = require("sequelize");
+
+// Cargamos la variables de entorno desde el .env
+process.loadEnvFile();
+const { DBUSER, PASSWORD, HOST, DATABASE } = process.env;
+
+// Configuramos Sequelize.
+const sequelize = new Sequelize(DATABASE, DBUSER, PASSWORD, {
+  host: HOST,
+  dialect: "mysql",
+  pool: {
+    max: 5, // Máximo de conexiones en el grupo.
+    min: 0, // Minimo de conexiones en el grupo.
+    acquire: 30000, // Tiempo máximo, para liberar conexiones inactivas.
+    idle: 10000, // Tiempo máximo para cerrar conexiones inactivas.
+  }, 
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
+// Función para probar la conexión a la base de datos.
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión exitosa con la base de datos.");
+  } catch (err) {
+    console.error("Error al conectar a la base de datos:", err);
+  } finally {
+    console.log(
+      "Finalizó el intento de conectar a MySQL. Verifique el resultado arriba."
+    );
+  }
+};
 
-module.exports = connection;
-    
+// Ejecutamos la función de prueba.
+testConnection();
+
+// Exportamos para usar en otros módulos.
+module.exports = { sequelize };
