0f8b32b965b4b997fad54ddbe7a5a8b9aa74377f
DiriARG <matidirisio@gmail.com>
2024-10-13 00:37:46 -0300

    Actualizamos

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const app = express();
-const contenidoRoutes = require('./routes/contenidoRoutes');
-const db = require('./conexion/database');
+// Importamos los módulos que utilizaremos (express y morgan).
+const express = require("express");
+const morgan = require("morgan");
 
-// Middlewares
+// Importamos las rutas definidas en otro archivo.
+const contenidoRoutes = require('./src/routes/contenidoRoutes');
+
+// Importamos la función para conectarse a la base de datos MySQL.
+const { sequelize } = require("./src/conexion/database");
+
+// Definimos el puerto.
+const PORT = process.env.PORT ?? 3008;
+
+// Creamos una instancia de la aplicación Express.
+const app = express();
+
+// Desactivamos el encabezado X-Powered-By por razones de seguridad.
+app.disable("x-powered-by");
+
+// Middlewares.
 app.use(express.json());
+app.use(morgan("dev"));
+
+// Rutas. 
 app.use('/contenido', contenidoRoutes);
 
-// Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+// Middleware para manejar rutas no encontradas.
+app.use((req, res) => { 
+  res.status(404).json({ error: "Ruta no encontrada 🚫❗" });
 });
-    
+
+// Inicializamos el servidor.
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en: http://localhost:${PORT}`);
+});
