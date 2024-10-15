64e6354c12687726d01413f1122ee261a2525d28
DiriARG <matidirisio@gmail.com>
2024-10-15 16:03:05 -0300

    Agregamos la ruta para obtener un contenido por ID.

--- src/routes/contenidoRoutes.js
+++ src/routes/contenidoRoutes.js
@@ -6,5 +6,8 @@ const contenidoController = require("../controllers/contenidoController");
 // Obtener todos los contenidos.
 router.get("/", contenidoController.obtenerTodosLosContenidos);
 
+// Obtener un contenido por ID.
+router.get("/:id",contenidoController.obtenerContenidoPorID);
+
 // Exportamos el router para que pueda ser utilizado en 'app.js' u otros archivos.
 module.exports = router;
