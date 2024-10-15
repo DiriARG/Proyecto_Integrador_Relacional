95a9afc143681b5ee606b711d208604ee0588b63
DiriARG <matidirisio@gmail.com>
2024-10-15 10:46:56 -0300

    Realizamos un archivo centralizado que contenga las asociaciones de cada modelo.

---
+++ src/models/asociaciones.js
@@ -0,0 +1,20 @@
+const { Actor } = require('./actor');
+const { Categoria } = require('./categoria');
+const { Contenido } = require('./contenido');
+const { Genero } = require('./genero');
+
+// Definimos las asociaciones entre los modelos.
+// Relaciona el modelo "Actor" con "Contenido" (relación muchos a muchos).
+Actor.associate({ Contenido });
+
+// Relaciona el modelo "Categoria" con "Contenido" (relación uno a muchos).
+Categoria.associate({ Contenido });
+
+// Relaciona el modelo "Contenido" con "Genero", "Actor" y "Categoria".
+Contenido.associate({ Genero, Actor, Categoria });
+
+// Relaciona el modelo "Genero" con "Contenido" (relación muchos a muchos).
+Genero.associate({ Contenido });
+
+// Exportamos los modelos con las relaciones ya establecidas para usarlos en otras partes del proyecto.
+module.exports = { Actor, Categoria, Contenido, Genero};
