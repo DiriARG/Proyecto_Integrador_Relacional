8962bdf853d6da7315058deaf3dc9a4a9c6ae37b
DiriARG <matidirisio@gmail.com>
2024-10-22 11:15:55 -0300

    Agregamos una explicación del archivo

--- src/models/asociaciones.js
+++ src/models/asociaciones.js
@@ -1,3 +1,14 @@
+/*Explicación del archivo: 
+Este archivo garantiza que, al cargar la aplicación, todas las relaciones estén configuradas y listas para su uso en cualquier operación que lo requiera, mientras que las definiciones en los modelos individuales permiten mantener el código organizado y claro, facilitando la documentación y el trabajo modular. 
+En conjunto, estas dos capas de definiciones aseguran que las relaciones estén bien gestionadas y disponibles sin causar errores en la aplicación.
+La creación de este mismo archivo fue la solución al error "SequelizeEagerLoadingError: Categoria is not associated to Contenido!".
+Este mismo surgía aun cuando importaba todos los modelos en "contenidoController.js":
+const { Categoria } = require('../models/categoria');
+const { Genero } = require('../models/genero');
+const { Actor } = require('../models/actor');
+const { Contenido } = require('../models/contenido');
+Por lo tanto se creo este archivo para que las relaciones esten inicializadas y sean conocidas por Sequelize antes de realizar consultas. 
+*/
 // Importa los modelos necesarios para establecer relaciones.
 const { Actor } = require('./actor');
 const { Categoria } = require('./categoria');
